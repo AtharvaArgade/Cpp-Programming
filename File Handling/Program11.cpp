e5b2d7864bd1fb25bb3a010e86ac8701beb59b36
AtharvaArgade <84590699+AtharvaArgade@users.noreply.github.com>
2021-06-27 01:08:50 +0530

    Add files via upload

---
+++ File Handling/Program11.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<unistd.h>
+#include<fcntl.h>
+
+// #define O_WRONLY 0x008
+// #define O_APPEND ox004
+using namespace std;
+
+class FileX
+{
+public:
+	int fd;
+
+	FileX(char Name[])  // FileX(char *Name)
+	{
+		fd=open(Name, O_WRONLY | O_APPEND);
+		if(fd==-1)
+		{
+			cout<<"Unable to open file\n";
+			exit(0);
+		}
+		else
+		{
+			cout<<"File successfully opened\n";
+		}
+	}
+
+	void WriteData()
+	{
+		char str[] = "Pune";
+		write(fd,str,4);
+	}
+
+	~FileX()
+	{
+		close(fd);
+	}
+};
+
+int main()
+{
+	char str[30];
+	cout<<"Enter file name\n";
+	cin>>str;
+	FileX obj(str);
+	obj.WriteData();
+	return 0;
+}
